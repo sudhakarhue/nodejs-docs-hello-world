8965e69bd8e2826deabbfd185289ae509f26e64b
Navaneethakrishnan <926202@cognizant.com>
2021-06-06 12:42:28 +0530

    console logs added

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((request, response) => {
+    console.log("Test 1");
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    console.log("Test 2");
+    response.end("Hello World Test!");  
+    console.log("Test 3");  
 });
 
 const port = process.env.PORT || 1337;
