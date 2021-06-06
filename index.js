5bc1685bd5ac7950fcd1f7a21eb81f06e8b1af82
sudhakarhue <82567301+sudhakarhue@users.noreply.github.com>
2021-06-06 12:41:39 +0530

    logs upadated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
+    console.log("Testing");
     response.end("Hello World!");
 });
 
